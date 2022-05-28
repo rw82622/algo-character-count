0ddb8026aca5c7e52cd5895ed6e4bd4f484c1a11
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-28 09:47:36 -0500

    added comments to js solution for characterCount challenge

--- js/charCount.js
+++ js/charCount.js
@@ -1,20 +1,24 @@
 exports.charCount = function (yourString) {
-  myString = yourString.replace(/\W/g, "").split("");
-  //   console.log(myString);
+  //initialize your variables
+  let myString = yourString.replace(/\W/g, "").split("");
   let myObj = {};
   let myArr = [];
+  //find the count of each unique letter in the string
   for (let item of myString) {
     myObj[item] ? (myObj[item] += 1) : (myObj[item] = 1);
   }
+  //add each unique letter and it's count as an array to myArr
   for (let key in myObj) {
     myArr.push([key, myObj[key]]);
   }
-  let alphaSortedArray = myArr.slice().sort((a, b) => {
+  //sort myArr alphabetically
+  myArr.sort((a, b) => {
     if (a[0] > b[0]) return 1;
     if (a[0] < b[0]) return -1;
     if (a[0] == b[0]) return 0;
   });
-  return alphaSortedArray.sort((a, b) => {
+  //return myArr sorted by the number of occurances (descending)
+  return myArr.sort((a, b) => {
     if (a[1] > b[1]) return -1;
     if (a[1] < b[1]) return 1;
     if (a[1] == b[1]) return 0;
