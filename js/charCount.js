95c0a1a7a048337c818f97f126772c1d2f67ea0c
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-28 09:28:19 -0500

    js solution for characterCount

--- js/charCount.js
+++ js/charCount.js
@@ -1,3 +1,23 @@
-exports.charCount = function() {
-
+exports.charCount = function (yourString) {
+  myString = yourString.replace(/\W/g, "").split("");
+  //   console.log(myString);
+  let myObj = {};
+  let myArr = [];
+  for (let item of myString) {
+    myObj[item] ? (myObj[item] += 1) : (myObj[item] = 1);
+  }
+  for (let key in myObj) {
+    myArr.push([key, myObj[key]]);
+  }
+  let alphaSortedArray = myArr.slice().sort((a, b) => {
+    if (a[0] > b[0]) return 1;
+    if (a[0] < b[0]) return -1;
+    if (a[0] == b[0]) return 0;
+  });
+  return alphaSortedArray.sort((a, b) => {
+    if (a[1] > b[1]) return -1;
+    if (a[1] < b[1]) return 1;
+    if (a[1] == b[1]) return 0;
+  });
 };
+// console.log(charCount("an apple a day will keep the doctor away"));
